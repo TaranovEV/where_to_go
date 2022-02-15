0704236ed59589dd652b4c03b9e850d2c4228c11
taranovev <tar_ev@mail.ru>
2022-02-15 19:01:46 +0300

    Add Place, Image model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200,
+                             verbose_name='Название')
+    placeId = models.CharField(max_length=200,
+                               verbose_name='Id')
+    description_short = models.CharField(max_length=300,
+                                         verbose_name='Краткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class Image(models.Model):
+    title = models.ForeignKey(Place,
+                              related_name='imgs',
+                              null=True,
+                              on_delete=models.SET_NULL)
+    image_number = (
+        models.PositiveSmallIntegerField(verbose_name='Номер изображения')
+    )
+    image = models.ImageField(verbose_name='Изображение')
+
+    def __str__(self):
+        return '{} {}'.format(self.image_number, self.title)
