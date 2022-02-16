5183377104305174acd8331be1d57d81f1a5073a
taranovev <tar_ev@mail.ru>
2022-02-16 20:11:48 +0300

    Add django-tinymce

--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
@@ -8,7 +9,7 @@ class Place(models.Model):
                                verbose_name='Id')
     description_short = models.CharField(max_length=300,
                                          verbose_name='Краткое описание')
-    description_long = models.TextField(verbose_name='Полное описание')
+    description_long = HTMLField(verbose_name='Полное описание')
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from where_to_go import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = ([
     path('admin/', admin.site.urls),
     path('', views.show_index),
+    path('tinymce/', include('tinymce.urls')),
     path('places/<place_id>/', views.show_place, name='place-geojson'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
