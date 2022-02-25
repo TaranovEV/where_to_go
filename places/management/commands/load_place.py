1328d7c5a41418418fc7d32f2226144328205d00
taranovev <tar_ev@mail.ru>
2022-02-25 13:07:34 +0300

    Check response

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -5,6 +5,11 @@ from django.core.files.base import ContentFile
 from places.models import Place, Image
 
 
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
 class Command(BaseCommand):
     help = 'Add place in DB'
 
@@ -15,18 +20,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         response = requests.get(options.get('json')).json()
+        check_for_redirect(response)
+        response.raise_for_status()
         place, created = Place.objects.get_or_create(
             title = response['title'],
             placeId = response['title'],
-            description_short = response['description_short'],
-            description_long = response['description_long'],
             lng = response['coordinates']['lng'],
             lat = response['coordinates']['lat'],
+            defaults={
+                'description_short': '',
+                'description_long': '',},
         )
         iter = 1
         for image_url in response['imgs']:
             filename = os.path.basename(image_url)
             image_response = requests.get(image_url)
+            check_for_redirect(image_response)
+            response.raise_for_status()
             obj, created = Image.objects.get_or_create(title=place, image_number=iter)
             obj.image.save(filename, ContentFile(image_response.content))
             obj.save()
