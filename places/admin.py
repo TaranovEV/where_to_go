bde89c76d811e1d0edbdb0cde81cd289a6ee9871
taranovev <tar_ev@mail.ru>
2022-03-26 21:25:47 +0300

    Replace mark_safe

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Place, Image
-from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from adminsortable2.admin import SortableInlineAdminMixin
 
@@ -13,10 +12,7 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     def preview_image(self, obj):
         return (
             format_html(
-                mark_safe(
-                    '<img src="{}" style="max-height: 200px;">'
-                ),
-                obj.image.url
+                    '<img src="{}" style="max-height: 200px;">', obj.image.url
             )
         )
 
